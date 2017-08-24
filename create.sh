e56c19a159e38108a1ab30dc3e78fee82b40e6e9
lazaax <lazaax@alienware-lazaax.antraxwall.anubis.lzx>
2017-08-23 20:20:51 -0500

    primer

---
+++ create.sh
@@ -0,0 +1,7 @@
+echo "# programacionWeb" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/lchavez17/programacionWeb.git
+git push -u origin master
+
